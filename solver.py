8b042202ff2aa96f9799a513602619a84a48782c
Michael Sparre <michaelsparre@berkeley.edu>
2021-05-02 21:52:07 -0700

    test1

--- solver.py
+++ solver.py
@@ -403,8 +403,8 @@ def solver4(G, c, k):
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
     inputs = glob.glob('inputs/large/*')
-    print(inputs)
-    
+    print(inputs[55:57])
+
     # num = 0
     # for input_path in inputs:
     #     output_path = 'outputs/large/' + basename(normpath(input_path))[:-3] + '.out'
